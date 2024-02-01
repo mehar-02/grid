8e8c6699c998675fbdfdf88f409d9e1e7b960397
mehar <mkaur_be20@thapar.edu>
2024-02-01 12:45:07 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+//import 'https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Poppins&display=swap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <div className="grid-item item1">Header</div>
+      <div className="grid-item item2">Sidebar</div>
+      <div className="grid-item item3">Content 1</div>
+      <div className="grid-item item4">Content 2</div>
+      <div className="grid-item item5">Content 3</div>
+      <div className="grid-item item6">Footer</div>
+    </div>
+  );
+}
+
+export default App;
